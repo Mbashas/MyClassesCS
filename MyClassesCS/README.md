a4bcc49e78d41dd9cb233334ee453e2ddc7df604
sethmbasha <sethmbasha@gmail.com>
2024-03-03 15:47:35 +0300

    initial

---
+++ MyClassesCS/README.md
@@ -0,0 +1,2 @@
+# MyClassesCS
+This is where I keep all my files at the moment
---
+++ MyClassesCS/fibonac.py
@@ -0,0 +1,7 @@
+def fib(n):
+    if n==2 or n==1:
+        result==1
+    else:
+        result=fib(n-1)+fib(n-2)
+    return result
+fib(5)
